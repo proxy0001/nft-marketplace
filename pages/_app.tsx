2f3c6411036e91a2046e72bdaf6e0220b2b798a2
proxy0001 <g.proxy0001@gmail.com>
2022-12-02 18:19:06 +0800

    init!: setup for chakra with next.js

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,9 @@
-import '../styles/globals.css'
+import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
+import theme from '../theme/theme'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <ChakraProvider theme={theme}>
+    <Component {...pageProps} />
+  </ChakraProvider>
 }
