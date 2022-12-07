b3d579e90424e48190c58c4bf720fb5216798bdf
proxy0001 <g.proxy0001@gmail.com>
2022-12-07 21:52:24 +0800

    chore: add react-query and dexie

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,19 @@
 // setup your wrapper in the _app file (e.g: pages/_app.js)
-import { Chakra } from "../components/Chakra";
+import { Chakra } from "../components/Chakra"
 import type { AppProps } from 'next/app'
 import theme from '../theme'
+import Wagmi from "../wagmi"
+import { QueryClient, QueryClientProvider, useQuery } from '@tanstack/react-query'
+const queryClient = new QueryClient()
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Chakra cookies={pageProps.cookies} theme={theme}>
-    <Component {...pageProps} />
-  </Chakra>
+  return (
+    <Wagmi>
+      <QueryClientProvider client={queryClient}>
+        <Chakra cookies={pageProps.cookies} theme={theme}>
+          <Component {...pageProps} />
+        </Chakra>  
+      </QueryClientProvider>
+    </Wagmi>
+  )
 }
